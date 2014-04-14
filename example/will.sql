25e497572cb0b1a28236af36832d045d14de906d
Elfet <anton@elfet.ru>
2014-04-14 20:07:18 +0400

    will.sql

---
+++ example/will.sql
@@ -0,0 +1,2 @@
+INSERT INTO profile (id, city, birth, age, tags) VALUES (null, "Saint Petersburg", "2014-04-14 20:03:56", 21, "one,two")
+INSERT INTO users (id, name, avatar, profile, friend) VALUES (null, "Anton", null, 10, 12)
